4b71c9da4bc7b94453bbb5ec0f1c09e582a78928
Martín Ramos <marprg@gmail.com>
2021-10-28 13:14:49 -0300

    Se comienza a loguear en las distintas entradas

--- app.py
+++ app.py
@@ -1,68 +1,70 @@
 import json
 from flask import Flask, request, jsonify, render_template_string
 from epsonproxy import cierreZ, descargar_reportes, ticket, ticket_no_fiscal, cierreX, pruebaTicket
+from proxy_logging import log_error, log_debug, log_info, log_error_input, log_error_output
+
+COMPROBANTE_FISCAL = "Comprobante Fiscal"
+COMPROBANTE_NO_FISCAL = "Comprobante no Fiscal"
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template_string('<h1>Conexión exitosa!</h1>')
+    return render_template_string('<h1>Conexión exitosa con el servidor!</h1>')
+
 
 @app.route('/ticket', methods=['POST'])
 def imprimirTicket():
     comprobante_json = request.json
     if(not comprobante_json):
-        return chequearJsonInput(comprobante_json)
+        return chequearJsonInput(comprobante_json, COMPROBANTE_FISCAL )
     res = json.loads(ticket(comprobante_json))
-    return chequearJsonOutput(res)
+    return chequearJsonOutput(res, COMPROBANTE_FISCAL, comprobante_json)
 
 @app.route('/ticketNoFiscal', methods=['POST'])
 def imprimirTicketNoFiscal():
     comprobante_json = request.json
     if(not comprobante_json):
-        return chequearJsonInput(comprobante_json)
+        return chequearJsonInput(comprobante_json, COMPROBANTE_NO_FISCAL)
     res = json.loads(ticket_no_fiscal(comprobante_json))
-    return chequearJsonOutput(res)
-
-@app.route('/testTicket', methods=['POST'])
-def imprimirTicketTest():
-    comprobante_json = request.json
-    if(not comprobante_json):
-        return chequearJsonInput(comprobante_json)
-    res = json.loads(pruebaTicket(comprobante_json))
-    return chequearJsonOutput(res)
+    return chequearJsonOutput(res, COMPROBANTE_NO_FISCAL, comprobante_json)
 
 @app.route('/cierreX', methods=['POST'])
 def imprimirCierreX():
     res = json.loads(cierreX())
-    return chequearJsonOutput(res)
+    return chequearJsonOutput(res, "CierreX")
 
 @app.route('/cierreZ', methods=['POST'])
 def imprimirCierreZ():
     res = json.loads(cierreZ())
-    return chequearJsonOutput(res)
+    return chequearJsonOutput(res, "CierreZ")
 
 @app.route('/descargarReportes', methods=['POST'])
 def descargarReportes():
     descargar_reportes()
     return "OK"
 
-def chequearJsonInput(req):
+## Chequeo de input válido
+def chequearJsonInput(req, metodo):
     response = app.response_class(
             response=json.dumps({"error": "Comprobante Incorrecto"}),
             status=500,
             mimetype='application/json')
+    log_error_input(req, metodo)
     return response
 
-def chequearJsonOutput(res):
+def chequearJsonOutput(res,  metodo, comprobante=None):
     if(res['con_errores'] == 1):
         response = app.response_class(
                 response=json.dumps({"error": res['descripcion']}),
                 status=500,
                 mimetype='application/json')
+        log_error_output(metodo, res, comprobante)
         return response
+        
     else:
         return jsonify(res)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
